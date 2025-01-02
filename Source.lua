9427d2c34aa8986a2422686535278e7be779ebdc
realredz9999 <lractwazabi@gmail.com>
2025-01-03 02:40:24 +0800

    Source.lua

--- Source.lua
+++ Source.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
-wait("3")
+wait("0.2")
 loadstring(game:HttpGet('https://darkscripts.space/scripts/131ff979-977f-424e-b9ef-88103b38bc60_886814449720229888.lua',true))()
